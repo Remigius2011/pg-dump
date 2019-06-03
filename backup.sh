9395dd413e93cddf18043a982f5f5ec0dd1405b4
Remigius Stalder <remigius.stalder@descom-consulting.ch>
2019-06-03 15:21:10 +0200

    set default protocol in all cases to https

--- backup.sh
+++ backup.sh
@@ -5,69 +5,53 @@
 export DUMP_FILE="$BACKUP_DIR/$PG_SCHEMA-$DB_ENV-$(date +"%F-%H%M%S").dump"
 
 if [ ! -d "$BACKUP_DIR" ]; then
-
   echo mkdir -p "$BACKUP_DIR"
   mkdir -p "$BACKUP_DIR"
-
 fi
 
 echo "pg_dump -v -w -Fc -h $PG_HOST -p $PG_PORT -n $PG_SCHEMA -U $PG_USER $PG_DB -f $DUMP_FILE"
 pg_dump -v -w -Fc -h $PG_HOST -p $PG_PORT -n $PG_SCHEMA -U $PG_USER $PG_DB -f $DUMP_FILE
 
-if [ -n "S3_HOST" ]; then
+if [ -n "$S3_HOST" ]; then
+  if [ -z "$S3_PROTOCOL" ]; then
+    export S3_PROTOCOL=https
+  fi
 
   export MC_HOST_store="$S3_PROTOCOL://$S3_ACCESS_KEY:$S3_SECRET_KEY@$S3_HOST"
-  echo "mc cp $DUMP_FILE store/$S3_BUCKET"
+  echo "mc cp $DUMP_FILE store/$S3_BUCKET on $S3_HOST"
   mc cp $DUMP_FILE store/$S3_BUCKET
 
   if [ -n "$BACKLOG" ]; then
-
     BACKLOG_FILE="$PG_SCHEMA-$DB_ENV-$(date --date="-$BACKLOG day" +"%F-%H%M%S").dump"
     echo "deleting files before $BACKLOG_FILE"
 
     for FILE in $(mc ls store/$S3_BUCKET | grep "$PG_SCHEMA-$DB_ENV-" | cut -d' ' -f7-); do 
-
       if [ "$FILE" \< "$BACKLOG_FILE" ]; then
-
         echo "delete file $FILE"
         mc rm "store/$S3_BUCKET/$FILE"
-
       fi
-
     done
-
   fi
-
 fi
 
 if [ -n "$S3_SYNC_HOST" ]; then
-
-  if [ -n "$S3_SYNC_PROTOCOL" ]; then
-
+  if [ -z "$S3_SYNC_PROTOCOL" ]; then
     export S3_SYNC_PROTOCOL=https
-
   fi
 
   export MC_HOST_sync="$S3_SYNC_PROTOCOL://$S3_SYNC_ACCESS_KEY:$S3_SYNC_SECRET_KEY@$S3_SYNC_HOST"
-  echo "mc cp $DUMP_FILE sync/$S3_BUCKET"
+  echo "mc cp $DUMP_FILE sync/$S3_BUCKET on $S3_SYNC_HOST"
   mc cp $DUMP_FILE sync/$S3_BUCKET
 
   if [ -n "$BACKLOG" ]; then
-
     BACKLOG_FILE="$PG_SCHEMA-$DB_ENV-$(date --date="-$BACKLOG day" +"%F-%H%M%S").dump"
     echo "deleting files before $BACKLOG_FILE"
 
     for FILE in $(mc ls sync/$S3_BUCKET | grep "$PG_SCHEMA-$DB_ENV-" | cut -d' ' -f7-); do 
-
       if [ "$FILE" \< "$BACKLOG_FILE" ]; then
-
         echo "delete file $FILE"
         mc rm "sync/$S3_BUCKET/$FILE"
-
       fi
-
     done
-
   fi
-
 fi
--- restore.sh
+++ restore.sh
@@ -7,25 +7,23 @@
 export DUMP_FILE="$BACKUP_DIR/$PG_SCHEMA-$DB_ENV-$1.dump"
 
 if [ ! -d "$BACKUP_DIR" ]; then
-
   echo mkdir -p "$BACKUP_DIR"
   mkdir -p "$BACKUP_DIR"
-
 fi
 
-if [ -n "S3_HOST" ]; then
+if [ -n "$S3_HOST" ]; then
+  if [ -z "$S3_PROTOCOL" ]; then
+    export S3_PROTOCOL=https
+  fi
 
   export MC_HOST_store="$S3_PROTOCOL://$S3_ACCESS_KEY:$S3_SECRET_KEY@$S3_HOST"
-  echo "mc cp store/$S3_BUCKET/$PG_SCHEMA-$DB_ENV-$1.dump $BACKUP_DIR"
+  echo "mc cp store/$S3_BUCKET/$PG_SCHEMA-$DB_ENV-$1.dump $BACKUP_DIR from $S3_HOST"
   mc cp store/$S3_BUCKET/$PG_SCHEMA-$DB_ENV-$1.dump $BACKUP_DIR
-
 fi
 
 if [ ! -f "$DUMP_FILE" ]; then
-
   echo "dump file $DUMP_FILE is not a file"
   exit 1
-
 fi
 
 echo "pg_restore -v -c -w -h $PG_HOST -p $PG_PORT -U $PG_USER -n $PG_SCHEMA -d $PG_DB $DUMP_FILE"
